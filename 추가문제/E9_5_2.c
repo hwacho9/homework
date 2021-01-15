95c03d14c9757c124b853f5904d74523100e0df9
hwacho9 <63436365+hwacho9@users.noreply.github.com>
2021-01-15 23:45:28 +0900

    Update E9_5_2.c

--- 추가문제/E9_5_2.c
+++ 추가문제/E9_5_2.c
@@ -7,28 +7,19 @@ int main()
 {
     char sentence[128] = "This is a pen. That is an apple.";
     char str[128];
-    char tmp[128];
 
     get_line(str, 128);
 
-    strcpy(tmp, sentence);
+    char *ptr = strstr(sentence, str);
 
-    char *ptr = strchr(sentence, str);
-
-    while (ptr != NULL)
+    if (ptr != NULL)
     {
-        printf("%s\n", ptr); // 검색된 문자열 출력
-        ptr = strchr(ptr + 1, 'a');
-    }
-
-    /*if (strcmp(tmp, str) == 0)
-    {
-        printf("始まっている");
+        printf("含まれている");
     }
     else
     {
-        printf("始まっていない");
-    }*/
+        printf("含まれていない");
+    }
 
     return 0;
 }
